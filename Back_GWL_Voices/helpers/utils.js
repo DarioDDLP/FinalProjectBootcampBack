131dcbc0c6456e8b160a451debe8385388570956
Ismael de la Poza <ismaelpzex@gmail.com>
2022-09-06 19:15:47 +0200

    terminamos de integrar las promesas de las querys hacia la base de datos y probamos que todo funciona

---
+++ Back_GWL_Voices/helpers/utils.js
@@ -0,0 +1,52 @@
+const jwt = require('jsonwebtoken');
+const dayjs = require("dayjs");
+
+
+/**
+ * @description Executes an sql statement and returns an array/object with the result
+ * @param {*} sql 
+ * @param {*} arr 
+ * @returns return a array of query
+ */
+
+
+const executeQuery = (sql, arr = []) => {
+    return new Promise((resolve, reject) => {
+        db.query(sql, arr, (err, result) => {
+            if (err) return reject(err);
+            resolve(result);
+        });
+    });
+}
+
+/**
+ * @description Executes an SQL statement and returns a single element or null if it does not exist
+ * @param {*} sql 
+ * @param {*} arr
+ * @returns return a first value or null of query.
+ */
+
+const executeQueryOne = (sql, arr = []) => {
+    return new Promise((resolve, reject) => {
+        db.query(sql, arr, (err, result) => {
+            if (err) reject(err);
+            resolve(result.length === 0 ? null : result[0]);
+        });
+    });
+}
+
+/**
+ * @description Create token for user.
+ * @param {*} user 
+ * @return return token
+ */
+
+const createToken = (user) => {
+    const obj = {
+        user_id: user.id,
+        exp_date: dayjs().add(2, "hours").unix(),
+    }
+    return jwt.sign(obj, process.env.TOKEN_DECODE,);
+}
+
+module.exports = { executeQuery, executeQueryOne, createToken };
--- Back_GWL_Voices/routes/api/login.js
+++ Back_GWL_Voices/routes/api/login.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { createToken } = require('../../helpers/utils');
+const router = express.Router();
+
+const Users = require('../../models/users.model')
 
 /* GET login listing. */
-router.get('/', function (req, res) {
-    res.status(200).json({ "prueba": "test" });
+router.post('/', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await Users.getByEmail(email);
+        if (!user) return res.status(404).json({ error: "invaild email or passord" });
+        const equals = password === user.password ? true : false;
+        if (!equals) return res.status(404).json({ error: "invaild email or passord" });
+        res.status(200).json({
+            user,
+            success: 'Login correcto',
+            token: createToken(user),
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 module.exports = router;
