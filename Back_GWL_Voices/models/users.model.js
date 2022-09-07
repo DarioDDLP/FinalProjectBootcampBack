13e5879d1be1fa3fce9c043116205cd7d56366af
Ismael de la Poza <ismaelpzex@gmail.com>
2022-09-07 11:06:12 +0200

    soluciono conflictos

--- Back_GWL_Voices/models/users.model.js
+++ Back_GWL_Voices/models/users.model.js
@@ -5,13 +5,11 @@ const getByEmail = (email) => {
     return executeQueryOne('select * from users where email = ?', [email]);
 };
 
-<<<<<<< HEAD
 const create = ({ name, surname, email, password }) => {
     return executeQuery('insert into users (name, surname, email, password) values (?, ?, ?, ?)', [name, surname, email, password]);
 }
 
 module.exports = { getByEmail, create };
-=======
 const getAll = () => {
     return executeQuery('select * from users')
 }
@@ -21,4 +19,3 @@ const getById = (id) => {
 }
 
 module.exports = { getByEmail, getAll, getById };
->>>>>>> b8c45bfc35b944803991b7327acee15d9ea08d80
