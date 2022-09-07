2fce8076b664e52e48d93ded0a2a2d6081f980ea
Ismael de la Poza <ismaelpzex@gmail.com>
2022-09-07 11:32:21 +0200

    añado ruta de members

--- Back_GWL_Voices/routes/api.js
+++ Back_GWL_Voices/routes/api.js
@@ -1,9 +1,11 @@
 const app = require('../app');
 const loginRouter = require('./api/login');
 const registerRouter = require('./api/register');
+const memberRouter = require('./api/member');
 const router = require('express').Router();
 
 router.use('/login', loginRouter);
 router.use('/register', registerRouter);
+router.use('/member', memberRouter);
 
 module.exports = router;
