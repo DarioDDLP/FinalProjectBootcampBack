a77d83a4eadacfc9866e48f9d2e7a525fbfc9275
Ismael de la Poza <ismaelpzex@gmail.com>
2022-09-12 13:50:38 +0200

    arreglo resset password

--- Back_GWL_Voices/routes/api/member.js
+++ Back_GWL_Voices/routes/api/member.js
@@ -22,9 +22,13 @@ router.get('/', async (req, res) => {
 router.get('/user', async (req, res) => {
 
     const user = req.user
-    console.log(user)
-    const resp = await Users.getById(user.id)
-    res.status(200).json(resp)
+    try {
+        const response = await Users.getById(user.id)
+        res.status(200).json(response)
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+
 
 });
 
@@ -55,30 +59,46 @@ router.put('/:id', async (req, res, next) => {
 });
 
 
-router.post('/new-password',
-    body('newPassword')
-        .exists()
-        .withMessage('Password is required')
-        .matches(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/)
-        .withMessage(`The password must be between 8 and 16 characters long, with at least one digit, at least one lowercase letter, and at least one uppercase letter.
-It can NOT have other symbols.`),
-    async (req, res) => {
-        const { newPassword } = req.body;
-        const { id } = req.body.user;
+// router.post('/new-password',
+//         body('newPassword')
+//             .exists()
+//             .withMessage('Password is required')
+//             .matches(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/)
+//             .withMessage(`The password must be between 8 and 16 characters long, with at least one digit, at least one lowercase letter, and at least one uppercase letter.
+//     It can NOT have other symbols.`),
+//     async (req, res) => {
+//         const { newPassword } = req.body;
+//         const { id } = req.body.user;
 
-        if (!newPassword) return res.status(400).json({ message: 'password required' });
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) return res.json(errors.mapped());
-        const passwordUpdated = bcrypt.hashSync(newPassword, 12);
-        try {
-            const user = await Users.getById(id);
-            user.password = newPassword;
-            await Users.updateUserPassword(user.id, passwordUpdated);
-            res.status(200).json({ message: 'Password has been updated' });
-        } catch (err) {
-            return res.status(400).json({ message: 'Sometimes goes wrong!' });
-        }
-    })
+//         if (!newPassword) return res.status(400).json({ message: 'password required' });
+//         const errors = validationResult(req);
+//         if (!errors.isEmpty()) return res.json(errors.mapped());
+//         const passwordUpdated = bcrypt.hashSync(newPassword, 12);
+//         try {
+//             const user = await Users.getById(id);
+//             user.password = newPassword;
+//             await Users.updateUserPassword(user.id, passwordUpdated);
+//             res.status(200).json({ message: 'Password has been updated' });
+//         } catch (err) {
+//             return res.status(400).json({ message: 'Sometimes goes wrong!' });
+//         }
+//     })
+
+router.post('/new-password', async (req, res) => {
+    const newPassword = req.body.newpassword;
+    const { id } = req.user;
+    console.log(newPassword, id);
+    if (!newPassword) return res.status(400).json({ message: 'password required' });
+    const passwordUpdated = bcrypt.hashSync(newPassword, 12);
+    try {
+        const user = req.user
+        await Users.updateUserPassword(user.id, passwordUpdated);
+        res.status(200).json({ message: 'Password has been updated' });
+    } catch (err) {
+        return res.status(400).json({ message: 'Sometimes goes wrong!' });
+    }
+
+})
 
 module.exports = router;
 
