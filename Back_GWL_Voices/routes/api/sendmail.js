d2c5cc27451a04c538f123bebe76189ced549a7e
Ismael de la Poza <ismaelpzex@gmail.com>
2022-09-13 10:34:42 +0200

    Funcionalidad, enviar email terminada

--- Back_GWL_Voices/routes/api/sendmail.js
+++ Back_GWL_Voices/routes/api/sendmail.js
@@ -1,9 +1,26 @@
 const router = require('express').Router();
+const { transporter } = require('../../config/mailer');
+const User = require('../../models/users.model')
 
-router.post('/:id', async (req, res) => {
-    const { id } = req.params
-    console.log(id);
-    res.json({ 'hola': 'hola' });
+router.post('/:idReceiver', async (req, res) => {
+    const { idReceiver } = req.params;
+    const { id } = req.user;
+    const { subject, text } = req.body
+    try {
+        const receiver = await User.getById(idReceiver);
+        const emitter = await User.getById(id)
+        await transporter.sendMail({
+            from: `${emitter.name} ${emitter.surname}`, // sender address
+            to: "thegordosmen@gmail.com", // list of receivers
+            subject: subject, // Subject line
+            // text: "Hello world?", // plain text body
+            html: `<h1>${emitter.name} ${emitter.surname} has sent you a message</h1><p>${req.body.text}</p>`, // html body
+        });
+        res.json({ success: 'email has been sent' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+
 })
 
 module.exports = router;
