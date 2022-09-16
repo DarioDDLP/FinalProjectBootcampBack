5893f0823302de909933bbe8c212c88f5715a69e
joseangelchacin <joseangelchacin@gmail.com>
2022-09-16 11:50:02 +0200

    peticion post para crear nuevo thread

--- Back_GWL_Voices/models/messenger.model.js
+++ Back_GWL_Voices/models/messenger.model.js
@@ -4,6 +4,10 @@ const getAll = () => {
     return executeQuery(`select * from threads where status = true`, [])
 }
 
+const newThread = (title, user_id, created_at) => {
+    return executeQuery('insert into threads (title, user_id, created_at) values (?, ?, ?)', [title, user_id, created_at])
+}
 
 
-module.exports = { getAll };
+
+module.exports = { getAll, newThread };
--- Back_GWL_Voices/routes/api/messenger.js
+++ Back_GWL_Voices/routes/api/messenger.js
@@ -16,5 +16,17 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    const { user_id, title, created_at } = req.body
+    console.log(title, user_id, created_at, 'sssssssssssssssssssssssssssss')
+    try {
+        const response = await Messenger.newThread(title, user_id, created_at)
+        console.log(response, 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+});
+
 
 module.exports = router;
